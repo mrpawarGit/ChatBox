120e75ee8112057dda036d0ba2cc8778bffebd86
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-09-28 21:57:55 +0530

    backend done

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,22 @@ const cors = require("cors");
 const connectToDB = require("./config/db");
 const authRoute = require("./routes/authRoute");
 const chatRoute = require("./routes/chatRoute");
+const statusRoute = require("./routes/statusRoute");
 const bodyParser = require("body-parser");
+const initializeSocket = require("./services/socketService");
+const http = require("http");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+// cors
+const corsOption = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
+
+app.use(cors(corsOption));
 
 // Middleware setup
 app.use(cors()); // Enable cors
@@ -16,12 +27,25 @@ app.use(express.json()); // Parse JSON requests
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded form data
 app.use(cookieParser()); // Parse cookies from incoming requests
 
+// create server
+const server = http.createServer(app);
+
+const io = initializeSocket(server);
+
+// apply socket middleware before routes
+app.use((req, res, next) => {
+  req.io = io;
+  req.socketUserMap = io.socketUserMap;
+  next();
+});
+
 // Connect to MongoDB
 connectToDB();
 
 // routes
 app.use("/api/auth", authRoute);
 app.use("/api/chat", chatRoute);
+app.use("/api/status", statusRoute);
 
 // Test route to check if server is running
 app.get("/test", (req, res) => {
@@ -44,6 +68,6 @@ app.use((err, req, res, next) => {
 });
 
 // server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server Running on http://localhost:${PORT}/`);
 });
