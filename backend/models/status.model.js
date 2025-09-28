5c57713e1747f1b2104732f7e5ec468202e279c9
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-09-28 19:26:58 +0530

    statusSchema added

---
+++ backend/models/status.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const statusSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
+  content: { type: String, required: true },
+  contentType: {
+    type: String,
+    enum: ["image", "video", "text"],
+    default: "text",
+  },
+  viewers: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+  expiresAt: { type: Date, required: true },
+});
+
+const StatusModel = mongoose.model("Status", statusSchema);
+
+module.exports = StatusModel;
